b1c9a96c6cdb575bd2db8af7c087ded6ebf24682
yashrajrao7 <yashrajrao7@gmail.com>
2026-03-01 21:33:57 +0530

    Updated dashboard

---
+++ js/dashboard.js
@@ -0,0 +1,12 @@
+// Sample Data (Replace with real backend data later)
+
+let patients = 120;
+let doctors = 35;
+let appointments = 87;
+let pendingAuthorisations = 14;
+
+// Set values dynamically
+document.getElementById("totalPatients").textContent = patients;
+document.getElementById("totalDoctors").textContent = doctors;
+document.getElementById("totalAppointments").textContent = appointments;
+document.getElementById("pendingAuth").textContent = pendingAuthorisations;
